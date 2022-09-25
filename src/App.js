8a5a4a0030424ff659381dc40eb5efe8bbe5d1f0
Qixia Luo <ccqxluo@gmail.com>
2022-09-25 14:22:25 -0400

    add todo layout structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <h1>TodoMatic</h1>
+      <form>
+        <h2>
+            <label htmlFor="add-new-item">What needs to be done?</label>
+        </h2>
+        <input type="text" id="add-new-ttem"></input>
+        <button type="submit" class="btn__add">Add</button>
+      </form>
+      <div class="filter">
+        <button type="button" class="btn__filter">All</button>
+        <button type="button" class="btn__filter">Active</button>
+        <button type="button" class="btn__filter">Completed</button>
+      </div>
+      <div class="tasks">
+        <h2>3 Tasks Remaining</h2>
+        <ul>
+          <li>  
+            <input type="checkbox" id="item1"></input>
+            <label htmlFor="item1">Eat</label>
+            <div class="item-actions">
+              <button type="button">Edit</button>
+              <button type="button">Delete</button>
+            </div>
+          </li>
+          <li>
+            <input type="checkbox" id="item1"></input>
+            <label htmlFor="item1">Eat</label>
+            <div class="item-actions">
+              <button type="button">Edit</button>
+              <button type="button">Delete</button>
+            </div>
+          </li>
+          <li>
+            <input type="checkbox" id="item1"></input>
+            <label htmlFor="item1">Eat</label>
+            <div class="item-actions">
+              <button type="button">Edit</button>
+              <button type="button">Delete</button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
