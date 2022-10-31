0f43155415da005cbff8833cc7c8f471d13802f7
Qixia Luo <ccqxluo@gmail.com>
2022-10-30 20:57:44 -0400

    create basic todo-list

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import AddTask from './Components/AddTask';
 import Tasks from './Components/Tasks';
 
 function App() {
+  <h1>TodoMatic</h1>
   return (
     <div className="todoapp">
-      <h1>TodoMatic</h1>
       <AddTask />
       <Tasks />
     </div>
--- src/Store/tasks-slice.js
+++ src/Store/tasks-slice.js
@@ -1,28 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const taskSlice = createSlice({
-    name: 'tasks',
-    initialState: {
-        tasks: [],
-        quantity: 0
-    }, 
-    reducers: {
-        addTask(state, action) {
-            state.quantity ++;
-            const newTask = action.payload;
-            state.tasks.push({
-                id: newTask.id,
-                title: newTask.title,
-                status: newTask.status
-            })
-        },
-        removeTask(state, action) {
-            state.quantity --;
-            const taskToRemove = action.payload;
-            state.tasks = state.tasks.filter((item) => item.id !== taskToRemove.id);
-        },
-    }
-})
+  name: "tasks",
+  initialState: {
+    tasks: [],
+    fquantity: 0,
+  },
+  reducers: {
+    addTask(state, action) {
+      state.quantity++;
+      const newTask = action.payload;
+      state.tasks.push({
+        id: newTask.id,
+        title: newTask.title,
+        status: newTask.status,
+      });
+    },
+    removeTask(state, action) {
+      state.quantity--;
+      const taskToRemove = action.payload;
+      state.tasks = state.tasks.filter((item) => item.id !== taskToRemove.id);
+    },
+  },
+});
 
 export const taskActions = taskSlice.actions;
 export default taskSlice;
